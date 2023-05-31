85fbdebac2590d8f77b52553a5d4e4654fa2693e
Irene Ann Jacob <85626468+IRENE260@users.noreply.github.com>
2023-05-30 22:21:21 -0700

    Create Adminprofile.php

---
+++ Controller/Admin/Adminprofile.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace  App\Controllers;
+
+class adhome extends BaseController
+{
+    public function ouptut()
+    {
+        return view('adminhome');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
