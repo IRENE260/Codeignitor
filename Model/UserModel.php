58a2f175de34757e37c4c732f40452051feb9cf2
Irene Ann Jacob <85626468+IRENE260@users.noreply.github.com>
2023-05-30 22:27:33 -0700

    Create UserModel.php

---
+++ Model/UserModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class userModel extends Model
+{
+    protected $table      = 'register';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['name','email', 'password','conpass','gender','phonenumber','filename','created_at','deleted_at','updated_at'];
+
+    //  Dates
+    protected $useTimestamps = false;
+     protected $dateFormat    = 'datetime';
+     protected $createdField  = 'created_at';
+     protected $updatedField  = 'updated_at';
+     protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      =[];
+    
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    }
