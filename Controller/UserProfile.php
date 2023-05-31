594f611a52ff45831f57c04349c602d1e4f86cb3
Irene Ann Jacob <85626468+IRENE260@users.noreply.github.com>
2023-05-30 22:14:24 -0700

    Create UserProfile.php

---
+++ Controller/UserProfile.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controllers;
+
+class profileuser extends BaseController
+{
+    public function index()
+    {
+        return view('userprofile');
+    }
+    
+     
+}     
+
+
+       
+
