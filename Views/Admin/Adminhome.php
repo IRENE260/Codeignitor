41ce0e4f8e572b0caf04bb73b9100d511a61ae90
Irene Ann Jacob <85626468+IRENE260@users.noreply.github.com>
2023-05-30 22:36:07 -0700

    Create Adminhome.php

---
+++ Views/Admin/Adminhome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<head>
+<style>
+        body{
+            text-align:center;
+        }
+    </style>
+    </head>
+
+<body style="background-color: burlywood">
+  <br>
+  <h2>WELCOME </h2>
+  
+         <form method="post" action= >
+
+        <button class="nav"  name="requestform" value="requestform"><a href='verifyrequest'> VERIFY REQUEST </button><br><br>
+       <button class="nav"  name="donorform" value="donorform"><a href ='verifydonor'>  VERIFY DONOR</button><br><br>
+    </body>    
+    <!-- <button class="nav"  name="donor" value="donor"><a href='donor'> BE A DONOR</button><br><br>
+          -->
+</html>
